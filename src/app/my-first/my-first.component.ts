d8fd544439262e2ccc62387e8129152161ca621d
sanjaylakum01 <sanjaylakum352@gmail.com>
2024-08-29 13:06:20 +0530

    changes

---
+++ src/app/my-first/my-first.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-first.component.html',
+  styleUrl: './my-first.component.css'
+})
+export class MyFirstComponent {
+
+}
